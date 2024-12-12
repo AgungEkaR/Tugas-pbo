c2ef5aa2c6c4437a69aee03589d106d36aba823e
AgungEkaR <162032469+AgungEkaR@users.noreply.github.com>
2024-12-12 20:21:38 +0700

    Delete Abstract Class directory

--- Abstract Class/Makhluk Hidup/MakhlukHidup.java
+++
@@ -1,8 +0,0 @@
-public abstract class MakhlukHidup {
-  public abstract void bernapas();
-  public abstract void berkembangBiak();
- 
-  public void makan() {
-      System.out.println("Makhluk hidup membutuhkan makanan.");
-  }
-}
